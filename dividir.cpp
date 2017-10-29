644bc2d3ee6c386fa1927e412c9cbb6042780515
Lucas Bekier <bekierlucas@gmail.com>
2017-10-28 22:37:00 -0300

    Actualizo asi es casi igual al otro

--- dividir.cpp
+++ dividir.cpp
@@ -4,24 +4,29 @@
 #include <cmath>
 #define tiempo std::chrono::high_resolution_clock::now
 using namespace std;
-int main () {
+int main (int argc, char* argv[]) {
 	long long int from = 1;
-	long long int to = 100000000;
-
+	long long int to = 1000;
+    if (argc >= 3) 
+    {
+        from = atoi(argv[1]);
+		to = atoi(argv[2]);
+    }
 	auto start = tiempo();
-	int lala = 0;
-	for(int i = 3; i < to; i++) {
-		for (int j = 2; j <= floor(sqrt(i)); j++) {
-			if (i % j == 0) {
-				lala++;
-				break;
-			}
-		}
+	if (from % 2 == 0) from++;
+	if (to % 2 == 0) to--;
+
+	int lala = 1;
+	for(int n = 3; n <= to + 1; n+=2) {
+		int factor = 3;
+		int raiz = sqrt(n);
+    	while ( n % factor && factor <= raiz ) factor += 2;
+    	if (n % factor != 0) lala ++;
 	}
 	auto end = tiempo();
 
 	cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count() / 1000000000.<< endl;
 	
-	std::cout << to - from - lala - 1 << "\n";
+	std::cout << lala << "\n";
 	return 0;
 }
